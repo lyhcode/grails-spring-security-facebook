d4de3ecf078af8a333c04728504203942b7e0c0e
lyhcode (Yan-hong Lin) <lyhcode@gmail.com>
2013-01-16 22:06:19 +0800

    token.redirectUri missing

--- src/groovy/com/the6hours/grails/springsecurity/facebook/FacebookAuthProvider.groovy
+++ src/groovy/com/the6hours/grails/springsecurity/facebook/FacebookAuthProvider.groovy
@@ -80,6 +80,7 @@ public class FacebookAuthProvider implements AuthenticationProvider, Initializin
                 }
 
                 if (!freshToken) {
+                    //freshToken =  facebookAuthUtils.getAccessToken(token.code)
                     freshToken =  facebookAuthUtils.getAccessToken(token.code, token.redirectUri)
                 }
 
